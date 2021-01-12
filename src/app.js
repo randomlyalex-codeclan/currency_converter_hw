7f1cd83a70557207b93b298cc7bb8c1980c8ba2d
Alex Dodd <alexdodd@gmail.com>
2021-01-12 21:10:17 +0000

    hello world boilerplate confirmed

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import Vue from 'vue';
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Vue({
+    el: "#app",
+    data: {
+    },
+    computed: {
+    }, 
+    mounted: {
+
+    },
+    methods:{
+    }
+  });
+});
